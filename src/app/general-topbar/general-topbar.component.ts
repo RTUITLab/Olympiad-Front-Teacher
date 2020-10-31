e1af642783fcf9c0c0e71e71b6e0ec213fd3c1d3
IvLaptev <ivlaptev13@ya.ru>
2020-10-31 17:23:41 +0300

    Added login

--- src/app/general-topbar/general-topbar.component.ts
+++ src/app/general-topbar/general-topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserStateService } from '../services/User/user-state.service';
 
 @Component({
   selector: 'app-general-topbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralTopbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userState: UserStateService) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/models/Users.ts
@@ -0,0 +1,9 @@
+export class User {
+    public id?: string;
+    public firstName?: string;
+    public email?: string;
+    public studentID?: string;
+    public password?: string;
+    public token?: string;
+    public roles?: string[];
+}
